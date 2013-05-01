9443ef17c37183dccd81c29ed0005940a8f0ba05
Graeme Stuart <ggstuart@gmail.com>
2013-05-01 21:57:18 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='pyagents',
+      version='0.5',
+      description='Python agent-based modelling framework',
+      author='Graeme Stuart',
+      author_email='ggstuart@gmail.com',
+      url='',
+      license='GPL',
+      packages=[
+        'pyagents'
+      ],
+     )
